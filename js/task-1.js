c687c312a3bedefa97cb6fe6532196cfd775d24c
angeange1 <149048056+angeange1@users.noreply.github.com>
2024-01-19 00:07:15 +0900

    commit1

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,15 @@
 'use strict'
 
-function makeTransaction(quantity, pricePerDroid) {
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
     const totalPrice = quantity * pricePerDroid
-    const makeMessage = `You ordered ${quantity} droids worth ${totalPrice} credits!`
-    return makeMessage;
+    let makeMessage
+    totalPrice > customerCredits ? makeMessage = "Insufficient funds!" : makeMessage = `You ordered ${quantity} droids worth ${totalPrice} credits!`
+    return makeMessage
 }
 
-console.log(makeTransaction(5, 3000))
-console.log(makeTransaction(3, 1000))
-console.log(makeTransaction(10, 500))
+console.log(makeTransaction(5, 3000, 23000))
+console.log(makeTransaction(3, 1000, 15000))
+console.log(makeTransaction(10, 5000, 8000))
+console.log(makeTransaction(8, 2000, 10000))
+console.log(makeTransaction(10, 500, 5000))
 
